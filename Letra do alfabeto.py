9c116ec0f7d873d75d806df3f857cba7c68f089e
MARCINHO 2TEC <164876868+MarcioDanielTECC@users.noreply.github.com>
2024-05-24 12:54:14 -0300

    Adicionando codigo
    Esse código da uma palavra quando o usuario da uma letra a ele.

---
+++ Letra do alfabeto.py
@@ -0,0 +1,30 @@
+
+letra = input("Digite uma letra do alfabeto: ")
+letra = letra.lower()
+
+if letra == 'a':
+    print("Avião")
+elif letra == 'b':
+    print("Bola")
+elif letra == 'c':
+    print("Carro")
+elif letra == 'd':
+    print("Dado")
+elif letra == 'e':
+    print("Elefante")
+elif letra == 'f':
+    print("Foca")
+elif letra == 'g':
+    print("Gato")
+elif letra == 'h':
+    print("Hipopótamo")
+elif letra == 'i':
+    print("Igreja")
+elif letra == 'J':
+    print("Janela")
+elif letra == 'u':
+    print("Uva")
+elif letra == 'k':
+    print("Kalunga")
+else:
+    print("Não conheço uma palavra que comece com essa letra.")
